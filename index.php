6f72d98a30515219600fe4fce538f5f6fb65c2f1
Timur Gabbasov <Tigabb@yandex.ru>
2023-10-24 20:55:45 +0500

    Работа с БД. Модели

--- index.php
+++ index.php
@@ -4,12 +4,27 @@
 //foreach($_GET as $key => $value){
 //    echo $key . " : " . $value . "<br>";
 //}
+    use Dotenv\Dotenv;
     use Framework\Container;
+    use Framework\DbConnection;
 
     if ( file_exists(dirname(__FILE__).'/vendor/autoload.php') ) {
         require_once dirname(__FILE__) . '/vendor/autoload.php';
     }
 
+    if (file_exists(".env"))
+    {
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->load(); //все параметры окружения помещаются в массив $_ENV
+        echo "Окружение загружено<p>";
+        // var_dump($_ENV);
+    }
+    else {
+        echo "Ошибка хагрузки ENV<br>";
+    }
+
+
     Container::getApp()->run();
+
 
     die();
